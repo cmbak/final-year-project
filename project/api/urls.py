579aa316bf90f5a8a8a4a6ee8ce220daedb21dc4
cmbak <chikmii@gmail.com>
2025-01-11 20:22:00 +0000

    Add styling to signup page

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path
-
 from api.views import user_list_create_view
+from django.urls import path
 
 urlpatterns = [
     path("users/", user_list_create_view, name="user-list-create"),
