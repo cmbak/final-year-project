4c029cf79c522fac685fa3db4e524d37725dd33a
cmbak <chikmii@gmail.com>
2025-01-14 21:02:34 +0000

    Fix email unique test and make email unique and not blank

--- project/api/models.py
+++ project/api/models.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     """Model representing system User"""
 
-    pass
+    email = models.EmailField(
+        verbose_name="email address",
+        unique=True,
+        max_length=255,
+        blank=False,  # Email required on forms
+    )
