f642e07e0d20c8f58d3f3794ba2a712cd18c5445
cmbak <chikmii@gmail.com>
2025-01-15 21:08:02 +0000

    Separate create_db_user function to custom_user function and standard_user fixture

--- project/api/tests/test_models.py
+++ project/api/tests/test_models.py
@@ -1,48 +1,31 @@
 import pytest
-from api.models import User
 from django.db.utils import IntegrityError
 
-USER_USERNAME = "bob"
-USER_EMAIL = "bob@gmail.com"
-USER_PASSWORD = "password1@"
-
-
-def create_db_user(
-    username: str = USER_USERNAME,
-    email: str = USER_EMAIL,
-    password: str = USER_PASSWORD,
-) -> User:
-    """
-    Create user instance with given username, email and password.
-    Uses default values if no arguments provided
-    """
-    return User.objects.create(username=username, email=email, password=password)
+from .conftest import custom_user
 
 
 @pytest.mark.django_db(True)
-def test_create_user():
+def test_create_user(standard_user, username, email, password):
     """Test user creation functionality with valid user details"""
-    user = create_db_user()
-    assert user.username == USER_USERNAME
-    assert user.email == USER_EMAIL
-    assert user.password == USER_PASSWORD
+    user = standard_user
+    assert user.username == username
+    assert user.email == email
+    assert user.password == password
 
 
 @pytest.mark.django_db(True)
-def test_unique_user_username():
+def test_unique_user_username(standard_user):
     """
     Test that creating a user with the same username as an existing user is not allowed
     """
-    create_db_user()
     with pytest.raises(IntegrityError):
-        create_db_user(email="john.doe@gmail.com")
+        custom_user(email="john.doe@gmail.com")
 
 
 @pytest.mark.django_db(True)
-def test_unique_user_email():
+def test_unique_user_email(standard_user):
     """
     Test that creating a user with the same email as an exisiting user is not allowed
     """
-    create_db_user()
     with pytest.raises(IntegrityError):
-        create_db_user(username="John Doe")
+        custom_user(username="John Doe")
