c86e87bf72c2c214c94378cf814484b3e09234c7
cmbak <chikmii@gmail.com>
2025-01-18 11:41:10 +0000

    Add tests for login

--- project/api/tests/test_views.py
+++ project/api/tests/test_views.py
@@ -35,14 +35,25 @@ def test_post_valid_signup(api_client: APIClient) -> None:
 
 
 # Login View
-def test_post_valid_login(
-    api_client: APIClient, username: str, email: str, password: str
-) -> None:
+
+
+@pytest.mark.django_db(True)
+def test_post_valid_login(api_client: APIClient) -> None:
     """
     Test that sending a POST request to the login page with valid user data
     returns a 303 response and redirects the user to the main page
     """
-    data = {"username": username, "email": email, "password": password}
+    # Create user to be logged in by signing up
+    api_client.post(
+        "/signup/",
+        {
+            "username": "new_user",
+            "email": "new_user@gmail.com",
+            "password": "password1@",
+        },
+    )
+    # Data for POST
+    data = {"username": "new_user", "password": "password1@"}
 
     response = api_client.post("/login/", data)
 
@@ -50,12 +61,38 @@ def test_post_valid_login(
     assert "new url" == "main page"
 
 
-def test_post_invalid_login(api_client: APIClient):
+invalid_logins = [
+    ("username", "", "This field may not be blank."),
+    ("", "password", "This field may not be blank."),
+    ("username", "password", "Invalid username or password"),
+]
+
+
+@pytest.mark.django_db(True)
+@pytest.mark.parametrize("username, password, expected", invalid_logins)
+def test_post_invalid_login(
+    api_client: APIClient, username: str, password: str, expected: str
+) -> None:
     """
     Test that sending a POST requets to the login page with invalid user data
     returs a 400 response and shows the correct error message
     """
-    assert True is False
+    data = {"username": username, "password": password}
+
+    response = api_client.post("/login/", data)
+    errors = response.data["errors"]
+    all_errors = []
+
+    if errors.get("username") is not None:
+        for e in errors.get("username"):
+            all_errors.append(str(e))
+
+    if errors.get("password") is not None:
+        for e in errors.get("password"):
+            all_errors.append(str(e))
+
+    assert response.status_code == 400
+    assert expected in all_errors
 
 
 # TODO e2e testing for specific form errors...
