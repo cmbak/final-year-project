01f277a528dfff0b174754b6481ed5b717f63fef
cmbak <chikmii@gmail.com>
2025-01-19 17:03:00 +0000

    Add authentication scheme and view for retrieving current user

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,6 +1,7 @@
-from api.views import user_list_create_view
+from api.views import user_list_create_view, current_user_view
 from django.urls import path
 
 urlpatterns = [
     path("users/", user_list_create_view, name="user-list-create"),
+    path("current-user/", current_user_view, name="current-user"),
 ]
