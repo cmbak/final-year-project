04999de13ec32e832040605af325f757cc39babb
cmbak <chikmii@gmail.com>
2025-01-19 15:19:43 +0000

    Create ProtectedRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route, BrowserRouter } from "react-router";
 import Dashboard from "./components/Dashboard";
 import Home from "./Home";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
