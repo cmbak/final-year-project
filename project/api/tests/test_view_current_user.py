e804d2ac1967e9d8b24078f7cdfc71a15f3904b4
cmbak <chikmii@gmail.com>
2025-01-19 17:25:09 +0000

    Allow unauthenticated requests to current-user endpoint
    Returns an empty dict if not authenticated

--- project/api/tests/test_view_current_user.py
+++ project/api/tests/test_view_current_user.py
@@ -6,20 +6,22 @@ from rest_framework.test import APIClient
 @pytest.mark.django_db(True)
 def test_get_current_user_auth(api_client: APIClient, standard_user: User) -> None:
     """
-    Test that a GET request from an authenticated user
+    Test that a GET request from an logged in user
     returns a 200 response and the user for the current session
     """
     api_client.force_authenticate(user=standard_user)
 
     response = api_client.get("/api/current-user/")
-    user = response.json()["user"]
 
     assert response.status_code == 200
-    assert user == standard_user.as_dict()
+    assert response.json()["user"] == standard_user.as_dict()
 
 
 def test_get_current_user_not_auth(api_client: APIClient) -> None:
-    """Test that a GET request from an unauthenticated user returns a 401 response"""
+    """
+    Test that a GET request from an anonymous user (i.e. not logged in) returns no user
+    """
     response = api_client.get("/api/current-user/")
 
-    assert response.status_code == 401
+    assert response.status_code == 200
+    assert response.json()["user"] == {}
