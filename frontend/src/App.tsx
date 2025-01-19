30ba3b8c7c6d3073ee7cb401e779132068b6be4c
cmbak <chikmii@gmail.com>
2025-01-19 15:52:32 +0000

    Upgrade react-query to 5.64.2 and install react-query devtools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,23 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import Dashboard from "./components/Dashboard";
 import Home from "./Home";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
