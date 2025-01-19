819c1a7ea4eab0edf46aa900ceea1777da811658
cmbak <chikmii@gmail.com>
2025-01-19 15:52:32 +0000

    Upgrade react-query to 5.64.2 and install react-query devtools

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -4,5 +4,6 @@ import { Outlet } from "react-router";
 
 export default function ProtectedRoute() {
   const auth = useContext(AuthContext);
+
   return auth ? <Outlet /> : <h1>You are not authenticated</h1>;
 }
