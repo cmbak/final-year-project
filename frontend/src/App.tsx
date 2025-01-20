6e0bd18edf3e1826b64ec4a080dc8f2a6cb78c1d
cmbak <chikmii@gmail.com>
2025-01-20 23:10:26 +0000

    Organise components into folders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router";
-import Dashboard from "./components/Dashboard";
-import Home from "./components/Home";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Home from "./components/Home/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import "./reset.css";
 import "./App.css";
 
