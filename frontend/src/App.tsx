a800882c7cccd8e22292bf60b992600615a19b2e
cmbak <chikmii@gmail.com>
2025-01-21 17:02:19 +0000

    Add styling to navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,6 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <Navbar />
-        <h1>Hi</h1>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route element={<ProtectedRoute />}>
