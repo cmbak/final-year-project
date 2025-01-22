3288115ebf13ea0ebf3077602f2826bed7ab8f5f
cmbak <chikmii@gmail.com>
2025-01-22 12:13:52 +0000

    Add utility function to wrap test component in BrowserRouter and QueryClientProvider

---
+++ frontend/src/test-utils.tsx
@@ -0,0 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { render, RenderOptions } from "@testing-library/react";
+import React, { ReactElement } from "react";
+import { BrowserRouter } from "react-router";
+
+// Needed for testing routing and query client
+// From testing-library docs https://testing-library.com/docs/react-testing-library/setup/#custom-render
+const wrapProviders = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+export const providerRenderer = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">, // Remove wrapper property from type since it's being defined here
+) => render(ui, { wrapper: wrapProviders, ...options });
