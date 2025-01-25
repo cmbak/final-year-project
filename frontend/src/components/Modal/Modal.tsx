b6de3e82dbc1d687fecb3e7a2ab309bec22fd694
cmbak <chikmii@gmail.com>
2025-01-25 15:10:25 +0000

    Allow modal to be used for form submissions
    Move main close button into form and make it a submit button if that's the case

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -4,10 +4,11 @@ import styles from "./Modal.module.css";
 type ModalProps = {
   children: React.JSX.Element;
   title: string;
-  onClick: () => void;
+  type: "normal" | "form";
+  action?: (formData: FormData) => void;
 };
 
-export default function Modal({ children, title, onClick }: ModalProps) {
+export default function Modal({ children, title, type, action }: ModalProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -26,16 +27,31 @@ export default function Modal({ children, title, onClick }: ModalProps) {
               <i className="bi bi-x"></i>
             </button>
             <h2 className={styles.header}>{title}</h2>
-            <div className={styles.content}>{children}</div>
-            <button
-              className={`btn btn-secondary ${styles.mainBtn}`}
-              onClick={() => {
-                onClick();
-                setIsOpen(false);
-              }}
-            >
-              {title}
-            </button>
+            {/* If modal is for a form, main close button is inside form and of type submit */}
+            {type === "form" ? (
+              <div className={styles.content}>
+                <form action={action}>
+                  {children}
+                  <button
+                    type="submit"
+                    className={`btn btn-secondary ${styles.mainBtn}`}
+                    // onClick={() => setIsOpen(false)}
+                  >
+                    {title}
+                  </button>
+                </form>
+              </div>
+            ) : (
+              <>
+                <div className={styles.content}>{children}</div>
+                <button
+                  className={`btn btn-secondary ${styles.mainBtn}`}
+                  onClick={() => setIsOpen(false)}
+                >
+                  {title}
+                </button>
+              </>
+            )}
           </div>
         </>
       )}
