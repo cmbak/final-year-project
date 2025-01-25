090cec8562b61f5d6085dfef1a506577eb623ac7
cmbak <chikmii@gmail.com>
2025-01-25 15:32:14 +0000

    Move modal open state to parent component

--- frontend/src/components/CreateCategory/CreateCategory.tsx
+++ frontend/src/components/CreateCategory/CreateCategory.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { instance } from "../../axiosConfig";
 import Modal from "../Modal/Modal";
 
 export default function CreateCategory() {
+  const [modalActive, setModalActive] = useState(false);
+
   function createCategory(formData: FormData) {
     instance.post(
       "/api/category/",
@@ -9,15 +12,30 @@ export default function CreateCategory() {
       { withXSRFToken: true },
     );
 
-    // Close modal
+    setModalActive(false);
   }
 
   return (
-    <Modal title="create category" type="form" action={createCategory}>
-      <div className="form-item">
-        <label htmlFor="create-category-name">name</label>
-        <input name="name" id="create-category-name" required />
-      </div>
-    </Modal>
+    <>
+      <button
+        className="btn btn-secondary"
+        onClick={() => setModalActive(true)}
+        type="button"
+      >
+        create category
+      </button>
+      <Modal
+        title="create category"
+        type="form"
+        action={createCategory}
+        active={modalActive}
+        setActive={setModalActive}
+      >
+        <div className="form-item">
+          <label htmlFor="create-category-name">name</label>
+          <input name="name" id="create-category-name" required />
+        </div>
+      </Modal>
+    </>
   );
 }
