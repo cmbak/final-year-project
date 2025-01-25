905804d93c84f6db3689511251a1d1178ebeaf03
cmbak <chikmii@gmail.com>
2025-01-25 18:56:09 +0000

    Create category modal creates new category in db

--- frontend/src/components/CreateCategory/CreateCategory.tsx
+++ frontend/src/components/CreateCategory/CreateCategory.tsx
@@ -1,15 +1,22 @@
 import { useActionState, useState } from "react";
 import { instance } from "../../axiosConfig";
 import Modal from "../Modal/Modal";
+import { fetchUser } from "../../utils/fetchUser";
+import { useQuery } from "@tanstack/react-query";
 
 export default function CreateCategory() {
   const [modalActive, setModalActive] = useState(false);
   const [state, formAction, isPending] = useActionState(createCategory, null);
+  const { data } = useQuery({ queryKey: ["user"], queryFn: fetchUser });
 
   function createCategory(prevState: unknown, formData: FormData) {
     const name = formData.get("name");
     instance
-      .post("/api/category/", { name }, { withXSRFToken: true })
+      .post(
+        "/api/categories/",
+        { name, user: data.id },
+        { withXSRFToken: true },
+      )
       .then((response) => {
         console.log(response);
         setModalActive(false);
