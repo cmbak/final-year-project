1786df2a7df9523b24ffed16424f768970d2a733
cmbak <chikmii@gmail.com>
2025-01-25 16:50:41 +0000

    Register Category, Label and Quiz models on admin site

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Category, Label, Quiz
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Category)
+admin.site.register(Label)
+admin.site.register(Quiz)
