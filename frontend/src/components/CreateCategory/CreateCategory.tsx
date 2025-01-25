649afde2108d29485653a6ee60dfad9b89bcff89
cmbak <chikmii@gmail.com>
2025-01-25 15:10:25 +0000

    Allow modal to be used for form submissions
    Move main close button into form and make it a submit button if that's the case

--- frontend/src/components/CreateCategory/CreateCategory.tsx
+++ frontend/src/components/CreateCategory/CreateCategory.tsx
@@ -2,22 +2,22 @@ import { instance } from "../../axiosConfig";
 import Modal from "../Modal/Modal";
 
 export default function CreateCategory() {
-  async function createCategory(formData: FormData) {
-    await instance.post(
+  function createCategory(formData: FormData) {
+    instance.post(
       "/api/category/",
       { name: formData.get("name") },
       { withXSRFToken: true },
     );
+
+    // Close modal
   }
 
   return (
-    <Modal title="create category" onClick={() => console.log("clicked")}>
-      <form action={createCategory}>
-        <div className="form-item">
-          <label htmlFor="name">name</label>
-          <input name="name" required />
-        </div>
-      </form>
+    <Modal title="create category" type="form" action={createCategory}>
+      <div className="form-item">
+        <label htmlFor="create-category-name">name</label>
+        <input name="name" id="create-category-name" required />
+      </div>
     </Modal>
   );
 }
