d79253070c2872850b408c8f0c96263755802eba
cmbak <chikmii@gmail.com>
2025-01-26 22:45:02 +0000

    Create endpoint for getting user's quizzes

--- project/api/urls.py
+++ project/api/urls.py
@@ -4,6 +4,7 @@ from api.views import (
     label_list_create_view,
     user_categories_view,
     user_list_create_view,
+    user_quizzes_view,
 )
 from django.urls import path
 
@@ -13,6 +14,7 @@ urlpatterns = [
         user_categories_view,
         name="user-categories",
     ),
+    path("users/<int:user_id>/quizzes/", user_quizzes_view, name="user-quizzes"),
     path("users/", user_list_create_view, name="user-list-create"),
     path("current-user/", current_user_view, name="current-user"),
     path("categories/", category_list_create_view, name="create-categories"),
