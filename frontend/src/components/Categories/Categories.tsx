4d88b076c6fe1526636baafb93f51dab0bf72c7a
cmbak <chikmii@gmail.com>
2025-01-26 21:02:40 +0000

    Move categories to Category component

---
+++ frontend/src/components/Categories/Categories.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchCategories } from "../../utils/fetchCategories";
+import { fetchUser } from "../../utils/fetchUser";
+import styles from "./Categories.module.css";
+
+type Category = {
+  id: number;
+  name: string;
+  user: number;
+};
+
+export default function Categories() {
+  const user = useQuery({ queryKey: ["user"], queryFn: fetchUser });
+  const { isPending, isError, data } = useQuery({
+    queryKey: ["categories"],
+    queryFn: () => fetchCategories(user.data.id),
+  });
+
+  if (isPending) {
+    return <h1>TEMP Loading...</h1>;
+  }
+
+  if (isError) {
+    return <h1>TEMP Error...</h1>;
+  }
+
+  return (
+    <div className={styles.categories}>
+      {data.map(({ id, name }: Category) => (
+        <div key={id} className={styles.category}>
+          <h2>{name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,42 +1,13 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
 import CreateModal from "../CreateModal/CreateModal";
 import styles from "./Dashboard.module.css";
-import { fetchCategories } from "../../utils/fetchCategories";
-import { fetchUser } from "../../utils/fetchUser";
-
-type Category = {
-  id: number;
-  name: string;
-  user: number;
-};
+import Categories from "../Categories/Categories";
 
 export default function Dashboard() {
-  const user = useQuery({ queryKey: ["user"], queryFn: fetchUser });
-  const { isPending, isError, data } = useQuery({
-    queryKey: ["categories"],
-    queryFn: () => fetchCategories(user.data.id),
-  });
-
-  if (isPending) {
-    return <h1>TEMP Loading...</h1>;
-  }
-
-  if (isError) {
-    return <h1>TEMP Error...</h1>;
-  }
-
   return (
     <main className={`flex flex-col ${styles.container}`}>
       <h1 className={styles.heading}>my quizzes</h1>
       {/* TODO Search bar, create quiz button here */}
-      {/* TODO sep component? */}
-      <div className={styles.categories}>
-        {data.map(({ id, name }: Category) => (
-          <div key={id} className={styles.category}>
-            <h2>{name}</h2>
-          </div>
-        ))}
-      </div>
+      <Categories />
       <div className={`flex ${styles.modalBtnContainers}`}>
         <CreateModal
           endpoint="/api/categories/"
