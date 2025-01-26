cdc18ab2ff5b9f86c34f0ac05c3e269be9f1945d
cmbak <chikmii@gmail.com>
2025-01-26 17:31:32 +0000

    Add basic layout to dashboard

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,19 +1,24 @@
 import CreateModal from "../CreateModal/CreateModal";
+import styles from "./Dashboard.module.css";
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <CreateModal
-        endpoint="/api/categories/"
-        inputId="create-category-name"
-        title="create category"
-      />
-      <CreateModal
-        endpoint="/api/labels/"
-        inputId="create-label-name"
-        title="create label"
-      />
-    </div>
+    <main className={styles.container}>
+      <h1 className={styles.heading}>my quizzes</h1>
+      {/* Search bar, create quiz button here */}
+      <div>{/* Quiz categories here */}</div>
+      <div className={styles.modalBtnContainers}>
+        <CreateModal
+          endpoint="/api/categories/"
+          inputId="create-category-name"
+          title="create category"
+        />
+        <CreateModal
+          endpoint="/api/labels/"
+          inputId="create-label-name"
+          title="create label"
+        />
+      </div>
+    </main>
   );
 }
