63caac8903eb017255239d36c04c08a04d9914ad
cmbak <chikmii@gmail.com>
2025-01-27 21:21:28 +0000

    Add useCategories hook to fetch logged in user's categories

--- frontend/src/components/Categories/Categories.tsx
+++ frontend/src/components/Categories/Categories.tsx
@@ -1,31 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchCategories } from "../../utils/fetchCategories";
-import { fetchUser } from "../../utils/fetchUser";
 import { Category } from "../../types";
 import styles from "./Categories.module.css";
 import Quizzes from "../Quizzes/Quizzes";
+import useCategories from "../../hooks/useCategories";
 
 export default function Categories() {
-  const user = useQuery({ queryKey: ["user"], queryFn: fetchUser });
-  const userId = user.data?.id;
-
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ["categories", userId], // Depends on userId
-    queryFn: () => fetchCategories(userId),
-    enabled: Boolean(userId), // Only calls queryfn in userId not undefined/null
-  });
+  const { isPending, isError, data, error, userId } = useCategories();
 
   if (isPending) {
     return <h1>TEMP Loading...</h1>;
   }
 
   if (isError) {
-    return <h1>TEMP Error {error.message}</h1>;
+    return <h1>TEMP Error {error?.message}</h1>;
   }
 
   return (
     <div className={styles.categories}>
-      {data.map(({ id, name }: Category) => (
+      {data?.map(({ id, name }: Category) => (
         <div key={id} className={styles.category}>
           <h2>{name}</h2>
           <Quizzes userId={userId} categoryId={id} />
---
+++ frontend/src/hooks/useCategories.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchCategories } from "../utils/fetchCategories";
+import { fetchUser } from "../utils/fetchUser";
+
+// Hook which fetches the categories for the logged in user
+export default function useCategories() {
+  const user = useQuery({ queryKey: ["user"], queryFn: fetchUser });
+  const userId = user.data?.id;
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["categories", userId], // Depends on userId
+    queryFn: () => fetchCategories(userId),
+    enabled: Boolean(userId), // Only calls queryfn in userId not undefined/null
+  });
+
+  return { isPending, isError, data, error, userId };
+}
