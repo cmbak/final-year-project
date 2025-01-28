079ec59f60c168a3057ae5e9d684afb64f45f2ee
cmbak <chikmii@gmail.com>
2025-01-28 20:32:13 +0000

    Change /api/labels/ to create view only

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,7 +1,7 @@
 from api.views import (
     category_list_create_view,
     current_user_view,
-    label_list_create_view,
+    label_create_view,
     user_categories_view,
     user_list_create_view,
     user_quizzes_view,
@@ -22,5 +22,5 @@ urlpatterns = [
     path("users/", user_list_create_view, name="user-list-create"),
     path("current-user/", current_user_view, name="current-user"),
     path("categories/", category_list_create_view, name="create-categories"),
-    path("labels/", label_list_create_view, name="label-list-create"),
+    path("labels/", label_create_view, name="label-create"),
 ]
