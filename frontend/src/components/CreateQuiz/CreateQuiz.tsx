b8104f38640782e4c2ee139cca7ac48cef538911
cmbak <chikmii@gmail.com>
2025-01-30 18:40:03 +0000

    Show errors on create quiz form

--- frontend/src/components/CreateQuiz/CreateQuiz.tsx
+++ frontend/src/components/CreateQuiz/CreateQuiz.tsx
@@ -1,10 +1,39 @@
+import { useActionState } from "react";
 import useCategories from "../../hooks/useCategories";
 import BackButton from "../BackButton/BackButton";
 import LabelSelect from "../LabelSelect/LabelSelect";
 import styles from "./CreateQuiz.module.css";
+import { instance } from "../../axiosConfig";
+import FormError from "../FormError/FormError";
+
+type error = string[];
+
+type FormErrors = {
+  category?: error;
+  labels?: error;
+  title?: error;
+  user?: error;
+};
 
 export default function CreateQuiz() {
   const { isPending, isError, data, error, userId } = useCategories(); // TODO pending error
+  const [state, formAction, formPending] = useActionState(createQuiz, {
+    errors: {} as FormErrors,
+  });
+
+  async function createQuiz(prevState: unknown, formData: FormData) {
+    try {
+      const response = await instance.post(
+        "/api/quizzes/",
+        {},
+        { withXSRFToken: true },
+      );
+    } catch (error: any) {
+      return {
+        errors: error.response.data.errors,
+      };
+    }
+  }
 
   return (
     <>
@@ -12,7 +41,7 @@ export default function CreateQuiz() {
         <BackButton />
         <h2>create quiz</h2>
       </div>
-      <form className={`flex flex-col ${styles.form}`}>
+      <form className={`flex flex-col ${styles.form}`} action={formAction}>
         <input
           name="video"
           id="file"
@@ -22,6 +51,7 @@ export default function CreateQuiz() {
         />
         <label className="form-item" htmlFor="category">
           category
+          <FormError error={state?.errors.category} />
           <select name="category" id="category" className="input">
             <option disabled>Please select a category</option>
             {data?.map(({ id, name }) => (
@@ -34,16 +64,19 @@ export default function CreateQuiz() {
         <label className="form-item">
           {/* TODO should this be a label? or a p*/}
           labels
+          <FormError error={state?.errors.labels} />
           <LabelSelect userId={userId} />
         </label>
         <label className="form-item">
           quiz name
+          <FormError error={state?.errors.title} />
           <input type="text" name="quiz-name" required maxLength={50} />
         </label>
         <input /* Change to button? */
           type="submit"
           className="btn btn-secondary"
           value="Create Quiz"
+          disabled={formPending}
         />
       </form>
     </>
