99096911d3d9c9461a0e784ddb48278fd7774509
cmbak <chikmii@gmail.com>
2025-02-03 16:10:14 +0000

    Create Question and Answer models

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Category, Label, Quiz, User
+from .models import Category, Label, Quiz, User, QuizLabels, Question, Answer
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Category)
 admin.site.register(Label)
 admin.site.register(Quiz)
+admin.site.register(QuizLabels)
+admin.site.register(Question)
+admin.site.register(Answer)
