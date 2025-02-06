9c59a4b127b85b720b71fd1b540b802809d85bc6
cmbak <chikmii@gmail.com>
2025-02-06 19:19:01 +0000

    Add 404 page

---
+++ frontend/src/components/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+import styles from "./NotFound.module.css";
+export default function NotFound() {
+  return (
+    <div className={`center-container ${styles.container}`}>
+      <span className={styles.error}>404</span>
+      <h1>Looks like the page you were looking for doesn't exist</h1>
+      <Link to="/" className={"hover-underline"}>
+        Go Home
+      </Link>
+    </div>
+  );
+}
