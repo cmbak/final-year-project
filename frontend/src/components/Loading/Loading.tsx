870ee5e7feaeb687dfc8e7c81ae19eff8eec3c98
cmbak <chikmii@gmail.com>
2025-02-09 18:05:09 +0000

    Add bottom text for loading spinner

--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -2,13 +2,15 @@ import styles from "./Loading.module.css";
 
 type LoadingProps = {
   text: string;
+  bottomText?: string;
 };
 
-export default function Loading({ text }: LoadingProps) {
+export default function Loading({ text, bottomText }: LoadingProps) {
   return (
-    <div>
-      <h1>{text}</h1>
+    <div className={styles.container}>
+      <h1 className={styles.text}>{text}</h1>
       <div className={styles.spinner}></div>
+      {bottomText && <h2 className={styles.bottomText}>{bottomText}</h2>}
     </div>
   );
 }
