66efda3272edba227e56b46f45ce4b6cc9a75bbf
cmbak <chikmii@gmail.com>
2025-03-16 17:57:52 +0000

    Remove refs to categories in tests

--- project/api/tests/test_api.py
+++ project/api/tests/test_api.py
@@ -2,7 +2,7 @@ import json
 from unittest.mock import MagicMock, patch
 
 import pytest
-from api.models import Category, Label, Question, Quiz, User
+from api.models import Label, Question, Quiz, User
 from django.urls import reverse
 from google.api_core.exceptions import TooManyRequests
 from rest_framework.test import APIClient
@@ -159,120 +159,7 @@ def test_post_user_password_missing_reqs(
     )
 
 
-# Categories API Endpoint
-@pytest.mark.django_db(True)
-def test_get_category_auth(api_client: APIClient, standard_user: User) -> None:
-    """
-    Test that a GET request from an authenticated user
-    returns a 200 response and lists the categories
-    """
-    api_client.force_authenticate(user=standard_user)
-
-    response = api_client.get("/api/categories/")
-    categories = response.json()["results"]
-
-    assert response.status_code == 200
-    assert len(categories) == Category.objects.count()
-
-
-@pytest.mark.django_db(True)
-def test_get_category_not_auth(api_client: APIClient, standard_user: User) -> None:
-    """
-    Test that a GET request from an unauthenticated user returns a 401 response
-    """
-    response = api_client.get("/api/categories/")
-
-    assert response.status_code == 401
-
-
-@pytest.mark.django_db(True)
-def test_post_category_auth(api_client: APIClient, standard_user: User) -> None:
-    """
-    Test that a POST request from an authenticated user with a valid category name
-    returns a 201 response and creates a Category with that name
-    """
-    data = {"name": "New Category", "user": standard_user.id}
-    api_client.force_authenticate(user=standard_user)
-
-    response = api_client.post("/api/categories/", data)
-
-    assert response.status_code == 201
-    assert Category.objects.filter(name=data["name"]).exists()
-
-
-@pytest.mark.django_db(True)
-def test_post_category_not_auth(api_client: APIClient, standard_user: User) -> None:
-    """
-    Test that a POST request from an unauthenticated user with a valid category name
-    returns a 401 response and doesn't create a new Category
-    """
-    data = {"name": "New Category", "user": standard_user.id}
-
-    response = api_client.post("/api/categories/", data)
-
-    assert response.status_code == 401
-    assert not Category.objects.filter(name=data["name"]).exists()
-
-
-@pytest.mark.django_db(True)
-@pytest.mark.parametrize(
-    "name, expected",
-    [
-        ("", "This field may not be blank."),
-        (
-            "a very long and invalid category name",
-            "Ensure this field has no more than 30 characters.",
-        ),
-    ],
-)
-def test_post_invalid_category(
-    api_client: APIClient, standard_user: User, name: str, expected: str
-) -> None:
-    """
-    Test that sending a POST request with an invalid category name
-    returns a 400 response and shows the correct error messages
-    """
-    data = {"name": name, "user": standard_user.id}
-    api_client.force_authenticate(user=standard_user)
-
-    response = api_client.post("/api/categories/", data)
-    errors = get_response_errors(response)
-
-    assert response.status_code == 400
-    assert expected in errors
-
-
 # Creating Quiz
-
-# TODO valid quiz
-
-# @pytest.mark.django_db(True)
-# def test_post_quiz_auth(
-#     standard_user: User,
-#     api_client: APIClient,
-# ) -> None:
-#     """
-#     Test that a POST request from an authenticated user with valid quiz details
-#     returns 201 and creates a quiz with those details
-#     """
-#     label_one = Label.objects.create(name="Label One", user=standard_user)
-#     label_two = Label.objects.create(name="Label Two", user=standard_user)
-#     category = Category.objects.create(name="Category", user=standard_user)
-
-#     data = {
-#         "title": "Test Quiz",
-#         "labels": [label_one.id, label_two.id],
-#         "category": category.id,
-#         "user": standard_user.id,
-#         "url": "fakeurl.co.uk",
-#     }
-#     api_client.force_authenticate(user=standard_user)
-
-#     response = api_client.post(reverse("quiz-create"), data)
-
-#     assert response.json() == 1
-#     assert response.status_code == 201
-#     assert Quiz.objects.count() == 1
 
 
 @pytest.mark.django_db(True)
@@ -345,12 +232,10 @@ def test_post_quiz_other_user_labels(
     labels created by another user returns 400 and doesn't create the quiz
     """
     other_user = custom_user(username="other_user", email="other.user@gmail.com")
-    category = Category.objects.create(name="Category", user=standard_user)
     label = Label.objects.create(name="other label", user=other_user)
     data = {
         "title": "Title",
         "labels": [label.id],
-        "category": category.id,
         "user": standard_user.id,
         "url": "fakeurl.co.uk",
     }
@@ -362,87 +247,6 @@ def test_post_quiz_other_user_labels(
     assert response.status_code == 400
     assert Quiz.objects.count() == 0
     assert "You must use labels which you have created." in errors
-
-
-@pytest.mark.django_db(True)
-def test_post_quiz_other_user_categories(
-    standard_user: User, api_client: APIClient
-) -> None:
-    """
-    Test that a POST request from an authenticated user with a quiz containing
-    a category created by another user returns 400 and doesn't create the quiz
-    """
-    other_user = custom_user(username="other_user", email="other.user@gmail.com")
-    category = Category.objects.create(name="Other Category", user=other_user)
-    data = {
-        "title": "Title",
-        "labels": [],
-        "category": category.id,
-        "user": standard_user.id,
-        "url": "fakeurl.co.uk",
-    }
-    api_client.force_authenticate(user=standard_user)
-
-    response = api_client.post(reverse("quiz-create"), data)
-    errors = get_response_errors(response.json())
-
-    assert response.status_code == 400
-    assert Quiz.objects.count() == 0
-    assert "You must use a category which you have created." in errors
-
-
-# Users/id/categories/ endpoint
-
-
-@pytest.mark.django_db(True)
-def test_get_user_catgories_auth(api_client: APIClient, standard_user: User) -> None:
-    """
-    Test that a GET request to the user categories endpoint with an authenticated user
-    returns 200 and their categories
-    """
-    Category.objects.create(name="standard category", user=standard_user)
-    api_client.force_authenticate(user=standard_user)
-
-    response = api_client.get(f"/api/users/{standard_user.id}/categories/")
-    cat_user_ids = {
-        category["user"] for category in response.json()
-    }  # Set of all the categories user ids
-
-    assert response.status_code == 200
-    assert (
-        len(response.json()) == Category.objects.filter(user=standard_user.id).count()
-    )
-    assert len(cat_user_ids) == 1  # Only one id...
-    assert standard_user.id in cat_user_ids  # ... which is user's id
-
-
-@pytest.mark.django_db(True)
-def test_get_user_catgories_not_auth(
-    api_client: APIClient, standard_user: User
-) -> None:
-    """
-    Test that a GET request to the user categories endpoint with an unauthorised
-    returns 401
-    """
-    response = api_client.get(f"/api/users/{standard_user.id}/categories/")
-
-    assert response.status_code == 401
-
-
-@pytest.mark.django_db(True)
-def test_get_user_catgories_other_user(
-    api_client: APIClient, standard_user: User
-) -> None:
-    """
-    Test that a GET request to the endpoint for A DIFFERENT user's categories
-    with an authorised user returns 403
-    """
-    other_id = standard_user.id + 1  # different id to standard_user's id
-    api_client.force_authenticate(user=standard_user)
-    response = api_client.get(f"/api/users/{other_id}/categories/")
-
-    assert standard_user.id != other_id
-    assert response.status_code == 403
 
 
 # User's labels
@@ -492,53 +296,6 @@ def test_get_user_labels_other_user(standard_user: User, api_client: APIClient) 
     response = api_client.get(f"/api/users/{standard_user.id+1}/labels/")
 
     assert response.status_code == 403
-
-
-# User's quizzes in specified category
-
-
-@pytest.mark.django_db(True)
-def test_get_user_quiz_by_category_auth(api_client: APIClient, quiz: Quiz) -> None:
-    """Test that a GET request to the user quiz-by-categories endpoint
-    with an authenticated user returns 200 and their quizzes"""
-    user = quiz.user
-    api_client.force_authenticate(user=user)
-
-    response = api_client.get(f"/api/users/{user.id}/categories/{quiz.category.id}/")
-    response_quiz = response.json()[0]
-
-    assert response.status_code == 200
-    assert len(response.json()) == 1
-    assert response_quiz["id"] == quiz.id
-    assert response_quiz["category"] == quiz.category.id
-
-
-@pytest.mark.django_db(True)
-def test_get_user_quiz_by_category_not_auth(api_client: APIClient, quiz: Quiz) -> None:
-    """Test that a GET request to the user quiz-by-categories endpoint
-    with an unauthenticated user returns 401"""
-    response = api_client.get(
-        f"/api/users/{quiz.user.id}/categories/{quiz.category.id}/"
-    )
-
-    assert response.status_code == 401
-
-
-@pytest.mark.django_db(True)
-def test_get_user_quiz_by_category_other_user(
-    api_client: APIClient, quiz: Quiz
-) -> None:
-    """Test that a GET request to the user quiz-by-categories endpoint
-    with an authenticated user returns 403 and no quizzes"""
-    api_client.force_authenticate(user=quiz.user)
-
-    response = api_client.get(
-        f"/api/users/{quiz.user.id+1}/categories/{quiz.category.id}/"
-    )
-
-    assert response.status_code == 403
-    assert len(response.json()) == 1
-    assert response.json()["error"] == "You cannot access this"
 
 
 # Endpoint for a User's Quiz
--- project/api/tests/test_models.py
+++ project/api/tests/test_models.py
@@ -1,5 +1,6 @@
 import pytest
-from api.models import Category, Label, Quiz, User
+from api.models import Label, Quiz, User
+from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 
 from .conftest import custom_user
@@ -44,50 +45,6 @@ def test_user_string(standard_user: User) -> None:
 
 
 @pytest.mark.django_db(True)
-def test_category_valid_fields(standard_user: None) -> None:
-    """Test category creation functionality with valid name and user"""
-    category = Category.objects.create(name="category", user=standard_user)
-    assert category.name == "category"
-    assert category.user == standard_user
-
-
-@pytest.mark.django_db(True)
-def test_category_name_unique(standard_user: User) -> None:
-    """Test that the name of a category should be unique irrespective of case"""
-    Category.objects.create(name="category", user=standard_user)
-    with pytest.raises(IntegrityError):
-        Category.objects.create(name="Category", user=standard_user)
-
-
-@pytest.mark.django_db(True)
-def test_category_deleted_on_user_deletion() -> None:
-    """
-    Test that a category gets deleted when the user its foreign key
-    references also gets deleted
-    """
-    user = custom_user(username="john_doe")
-    Category.objects.create(name="Fun Category", user=user)
-
-    user.delete()
-
-    assert not User.objects.filter(username="john_due").exists()
-    assert not Category.objects.filter(name="Fun Category").exists()
-    assert Category.objects.count() == 0
-
-
-@pytest.mark.django_db(True)
-def test_category_string(standard_user: User) -> None:
-    """
-    Test that the Category string method returns the name of the category
-    """
-    category = Category.objects.create(name="Fun Category", user=standard_user)
-    assert str(category) == category.name
-
-
-# TODO what happens to related model instances when category is deleted?
-
-
-@pytest.mark.django_db(True)
 def test_label_valid_fields(standard_user: User) -> None:
     """Test label creation functionality with valid label name and user"""
     label = Label.objects.create(name="label", user=standard_user)
@@ -115,7 +72,6 @@ def test_quiz_valid_fields(quiz: Quiz, username: str) -> None:
     """Test quiz creation functionality with valid quiz fields"""
     assert quiz.title == "quiz"
     assert quiz.user.username == username
-    assert quiz.category.name == "category"
     assert quiz.labels.filter(name="label", user=quiz.user).exists()
 
 
@@ -132,7 +88,7 @@ def test_label_deleted_on_user_deletion(standard_user: User) -> None:
 @pytest.mark.django_db(True)
 def test_quiz_string(quiz: Quiz, username: str) -> None:
     """Test quiz string method returns the details of the quiz"""
-    assert str(quiz) == f"quiz | category | label | {username}"
+    assert str(quiz) == f"quiz | label | {username}"
 
 
 @pytest.mark.django_db(True)
@@ -140,11 +96,10 @@ def test_quiz_unique_title(quiz: Quiz) -> None:
     """Test that title of a quiz should be unique irrespective of case"""
     user = custom_user("new_user", "new_user@gmail.com")
 
-    with pytest.raises(IntegrityError):
+    with pytest.raises(ValidationError):
         Quiz.objects.create(
             title="quiz",
             user=user,
-            category=Category.objects.create(name="category two"),
         )
 
 
@@ -170,19 +125,3 @@ def test_quiz_deleted_on_user_deletion(quiz: Quiz) -> None:
 
     assert not Quiz.objects.filter(title="quiz").exists()
     assert Quiz.objects.count() == 0
-
-
-@pytest.mark.django_db(True)
-def test_quiz_category_null_on_category_deletion(quiz: Quiz) -> None:
-    """
-    Test that after a category is deleted, their quizzes should also be deleted
-    """
-    category = quiz.category
-
-    category.delete()
-
-    assert not Quiz.objects.filter(title="quiz").exists()
-    assert Quiz.objects.count() == 0
-
-
-# TODO after mvp maybe allow null categories?
