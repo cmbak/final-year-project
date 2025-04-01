97b42507bedee816a486ab134632b5b828deb21a
cmbak <chikmii@gmail.com>
2025-04-01 17:26:19 +0100

    Add embed url as property of youtube quiz

--- project/download_video.py
+++ project/download_video.py
@@ -1,13 +1,13 @@
 from pytubefix import YouTube
 
 
-def download_video(url: str) -> str:
+def download_video(url: str) -> tuple[str, str]:
     """
-    Download YT video from url, save in media directory and return file name of mp4
+    Download YT video from url, save in media directory and return tuple consisting of
+    file name of mp4 and embed url for video
     """
     yt = YouTube(url)
     print(yt.title)
     ys = yt.streams.get_lowest_resolution()
     file_path = ys.download("./media")
-    print(file_path)
-    return file_path
+    return (file_path, yt.embed_url)
