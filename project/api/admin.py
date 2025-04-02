b0578746f3618e8edf92b4666b9cab3c823b5956
cmbak <chikmii@gmail.com>
2025-04-02 13:33:33 +0100

    Delete label model and its references

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Answer, Label, Question, Quiz, QuizLabels, User
+from .models import Answer, Question, Quiz, User
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Label)
 admin.site.register(Quiz)
-admin.site.register(QuizLabels)
 admin.site.register(Question)
 admin.site.register(Answer)
