9e516031da93178b1617578b994cc871a6c75e40
cmbak <chikmii@gmail.com>
2025-04-29 17:28:28 +0100

    Show username on logout button

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
                 queryClient.invalidateQueries({ queryKey: ["user"] });
               }}
             >
-              logout
+              logout {data?.username}
             </button>
           </>
         )}
