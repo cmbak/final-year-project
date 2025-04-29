d5fbbccacf4d79e539f375f77d2609290d81ed34
cmbak <chikmii@gmail.com>
2025-04-29 17:02:57 +0100

    Scroll to video when timestamp clicked

--- frontend/src/components/Video/Video.tsx
+++ frontend/src/components/Video/Video.tsx
@@ -1,16 +1,33 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import { StateSetter } from "../../types";
 
 type VideoProps = {
   name: string;
   timestamp: number;
+  showCorrect: boolean;
+  timestampClicked: boolean;
+  setTimestampClicked: StateSetter<boolean>;
 };
 
-export default function Video({ name, timestamp }: VideoProps) {
+export default function Video({
+  name,
+  timestamp,
+  showCorrect,
+  timestampClicked,
+  setTimestampClicked,
+}: VideoProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  if (videoRef.current !== null) {
-    videoRef.current.currentTime = timestamp;
-  }
+  // In use effect so can set state of timestampClicked AFTER timestamp has been clicked changed in Question (i.e. when new value of timestamp set)
+  useEffect(() => {
+    if (videoRef.current !== null && showCorrect && timestampClicked) {
+      videoRef.current.currentTime = timestamp;
+      videoRef.current.scrollIntoView({ behavior: "smooth" });
+      videoRef.current.play();
+      // So that video timestamp can be changed again
+      setTimestampClicked(false);
+    }
+  }, [timestamp, showCorrect]);
 
   return (
     <video controls width="100%" height="100%" ref={videoRef}>
