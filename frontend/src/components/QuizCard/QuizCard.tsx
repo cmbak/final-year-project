bc240b33a13680df3373e05e8589b7fad5075c85
cmbak <chikmii@gmail.com>
2025-04-29 20:18:51 +0100

    Show modal when delete button pressed

--- frontend/src/components/QuizCard/QuizCard.tsx
+++ frontend/src/components/QuizCard/QuizCard.tsx
@@ -1,16 +1,42 @@
 import { Link } from "react-router";
 import styles from "./QuizCard.module.css";
 import { Quiz } from "../../types";
+import Modal from "../Modal/Modal";
+import { SetStateAction, useState } from "react";
 
 type QuizCardProps = Pick<Quiz, "id" | "title" | "thumbnail_url">;
 
 export default function QuizCard({ id, title, thumbnail_url }: QuizCardProps) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <Link to={`../take-quiz/${id}`}>
+    <>
+      <Modal
+        title="Delete Quiz"
+        type={"normal"}
+        active={showModal}
+        setActive={setShowModal}
+        margin={"0 -5rem"} // To counteract 5rem of inline padding on div container in Quizzes
+      >
+        <h1>hi</h1>
+      </Modal>
+      <div className={styles.container}>
         <img src={thumbnail_url} />
-        <div className={styles.details}>{title}</div>
-      </Link>
-    </div>
+        <div className={styles.details}>
+          <span>{title}</span>
+          <div className={styles.btns}>
+            <Link to={`../take-quiz/${id}`}>
+              <button className="btn btn-primary">Start</button>
+            </Link>
+            <button
+              onClick={() => setShowModal((prevShowModal) => !prevShowModal)}
+              className={styles.trash}
+            >
+              <i className="bi bi-trash-fill"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
