65b50d67ecf7c28dccdace57f5c05e4875e27111
cmbak <chikmii@gmail.com>
2025-05-04 19:56:04 +0100

    Center attempts chart

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -4,6 +4,8 @@ import useUser from "../../hooks/useUser";
 import { fetchQuizzes } from "../../utils/fetchQuizzes";
 import { Quiz } from "../../types";
 import StatsCard from "../StatsCard/StatsCard";
+import { fetchAttempts } from "../../utils/fetchAttempts";
+import AllAtempts from "../Graphs/AllAttempts";
 
 export default function Dashboard() {
   const user = useUser();
@@ -14,6 +16,12 @@ export default function Dashboard() {
     enabled: Boolean(userId),
   });
 
+  const attemptsData = useQuery({
+    queryKey: ["all_attempts", userId],
+    queryFn: () => fetchAttempts(userId),
+    enabled: Boolean(userId),
+  });
+
   if (isPending) {
     return;
   }
@@ -21,6 +29,18 @@ export default function Dashboard() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Dashboard</h1>
+      {attemptsData.isError ? (
+        <h1>Error {error && error.message}</h1>
+      ) : (
+        <div className={styles.attemptsGraph}>
+          <h2>Number of attempts by Quiz</h2>
+          {/* <p>Only for attempted quizzes</p> */}
+          <div className={styles.attemptsContainer}>
+            <AllAtempts attempts={attemptsData.data} quizzes={data} />
+          </div>
+        </div>
+      )}
+      <h1 className={styles.statsTitle}>Stats for each Quiz</h1>
       <div className={!isError ? styles.statsGrid : ""}>
         {isError ? (
           <h1>Error {error && error.message}</h1>
